c8d828ff7e163462b4a46cda3106a4820b15e684
Jakub Zikmund <work@jakubzikmund.eu>
2025-08-22 22:23:04 +0200

    Initial commit

---
+++ CSharpPlayground.Core/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace CSharpPlayground.Core;
+
+public class Class1
+{
+}
---
+++ CSharpPlayground.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace CSharpPlayground.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
